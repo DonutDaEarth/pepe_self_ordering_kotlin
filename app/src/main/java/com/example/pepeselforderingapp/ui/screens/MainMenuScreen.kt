76e0f08d712f3728564e003789645f0001537068
DonutDaEarth <ss1.kurniawan@gmail.com>
2025-12-18 17:29:19 +0700

    Revert "refactor cart functionality; add quantity management and toggle between Add To Cart and counter display"
    This reverts commit fa357608e079e2fa4cd8c141a5e9cdf8a687d310.

--- app/src/main/java/com/example/pepeselforderingapp/ui/screens/MainMenuScreen.kt
+++ app/src/main/java/com/example/pepeselforderingapp/ui/screens/MainMenuScreen.kt
@@ -259,19 +259,19 @@ fun MainMenuScreen(
                 },
                 onAddToCart = { selectedSubitems, quantity ->
                     // Add to cart with selected subitems
-                    if (quantity > 0) {
-                        cartViewModel?.addToCart(
-                            CartItemData(
-                                name = selectedMenuDetail!!.name,
-                                description = selectedMenuDetail!!.description,
-                                basePrice = selectedMenuDetail!!.basePrice,
-                                selectedSubitems = selectedSubitems,
-                                quantity = quantity,
-                                imageRes = selectedMenuDetail!!.imageRes
-                            )
+                    cartViewModel?.addToCart(
+                        CartItemData(
+                            name = selectedMenuDetail!!.name,
+                            description = selectedMenuDetail!!.description,
+                            basePrice = selectedMenuDetail!!.basePrice,
+                            selectedSubitems = selectedSubitems,
+                            quantity = quantity,
+                            imageRes = selectedMenuDetail!!.imageRes
                         )
-                    }
-                    // Note: If quantity is 0, we don't add to cart (user decremented to 0)
+                    )
+                    showMenuDetail = false
+                    selectedMenuItem = null
+                    selectedMenuDetail = null
                 }
             )
         }
